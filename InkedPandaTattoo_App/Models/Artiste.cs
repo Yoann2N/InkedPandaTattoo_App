250c3130431837671ecdaeba46d2bf69fe957be0
yoann <yoanngervais.yg13750@gmail.com>
2026-02-02 09:16:03 +0100

    Ajoutez des fichiers projet.

---
+++ InkedPandaTattoo_App/Models/Artiste.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace InkedPandaTattoo_App.Models
+{
+
+    public class Artiste
+    {
+        public long Id { get; set; }
+
+        public string Profession { get; set; } = string.Empty;
+        public string Style { get; set; } = string.Empty;
+
+        public string? Pseudo { get; set; }
+        public string? Bio { get; set; }
+
+        public string? Instagram { get; set; }
+        public string? Facebook { get; set; }
+
+        public string? BanniereUrl { get; set; }
+        public string? VignetteUrl { get; set; }
+
+        public string? Css { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public long UserId { get; set; }
+    }
+
+}
